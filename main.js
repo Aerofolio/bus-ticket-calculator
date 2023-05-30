bfa8393925f8a403885ed48d0139b6381297a055
Paulo Bodaneze Reva <paulo.reva@acad.ftec.com.br>
2023-05-30 20:44:21 -0300

    button function

--- main.js
+++ main.js
@@ -10,9 +10,71 @@ function initializeForm(){
 
 function addCalculateButtonFunction(){
     document.getElementById('calculate').onclick = () => {
+        let startDate = removeTime(document.getElementById('userStartDate').valueAsDate),
+            endDate = removeTime(document.getElementById('userEndDate').valueAsDate),
+            controlDate = new Date(startDate),
+            //daysBetween = daysBetweenTwoDates(endDate, startDate)
+            daysOcurrences = [0, 0, 0, 0, 0, 0, 0];
 
+        while(controlDate.getTime() != endDate.getTime()){
+            daysOcurrences[controlDate.getDay()]++;
+            controlDate.setDate(controlDate.getDate() + 1);
+            if(!(controlDate.getTime() != endDate.getTime()))
+            daysOcurrences[controlDate.getDay()]++;
+        }
+
+        let totalTickets = 0;
+        for(let i = 0; i < daysOcurrences.length; i++){
+            let dayId = '';
+
+            switch(i){
+                case 0:
+                    dayId = 'sun';
+                    break;
+                case 1:
+                    dayId = 'mon';
+                    break;
+                case 2:
+                    dayId = 'tue';
+                    break;
+                case 3:
+                    dayId = 'wed';
+                    break;
+                case 4:
+                    dayId = 'thu';
+                    break;
+                case 5:
+                    dayId = 'fri';
+                    break;
+                case 6:
+                    dayId = 'sat';
+                    break;
+            }
+
+            totalTickets += Number(document.getElementById(dayId).value) * daysOcurrences[i];
+        }
+
+        totalTickets += Number(document.getElementById('extras').value);
+
+        let finalValue = totalTickets * Number(document.getElementById('ticketValue').value.replace(',', '.'));
+        finalValue -= Number(document.getElementById('cardValue').value.replace(',', '.'));
+
+        document.getElementById('finalValueFormated').innerHTML = finalValue.toString().replace('.', ',');
     };
 }
+
+function removeTime(date = new Date()) {
+    return new Date(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate()
+    );
+}
+
+// function daysBetweenTwoDates(dateA, dateB){
+//     let difference = dateA.getTime() - dateB.getTime();
+//     return Math.ceil(difference / (1000 * 3600 * 24));
+// }
 
 initializeForm();
 addCalculateButtonFunction();
